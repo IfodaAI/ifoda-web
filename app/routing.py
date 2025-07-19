90e32e6292def87f255cba1ff1f163a114239549
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-07-19 09:43:45 +0500

    routing.py update

--- app/routing.py
+++ app/routing.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:order_id>/', consumers.ChatConsumer.as_asgi()),
+    # path('ws/chat/<str:order_id>/', consumers.ChatConsumer.as_asgi()),
     path('ws/notification/', consumers.NotificationConsumer.as_asgi()),
-    re_path(r'ws/orders/(?P<order_id>[0-9a-f-]+)/$', consumers.NewChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<order_id>[0-9a-f-]+)/$', consumers.NewChatConsumer.as_asgi()),
 ]

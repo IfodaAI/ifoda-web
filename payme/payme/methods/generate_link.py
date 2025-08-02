6ff3907822cbb841a2259a70472e055cdee61845
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-08-02 11:54:48 +0500

    GeneratePayLink update

--- payme/payme/methods/generate_link.py
+++ payme/payme/methods/generate_link.py
@@ -22,8 +22,8 @@ class GeneratePayLink:
             payme_id=os.getenv("PAYME_ID"),
             payme_account=os.getenv("PAYME_ACCOUNT"),
             order_id=self.order_id,
-            amount=float(self.amount)*100,
-            return_url=return_url
+            amount=float(self.amount)*100
+            # return_url=return_url
         )
         encode_params = base64.b64encode(params.encode("utf-8"))
         print('link_params: ',params)
